1cadf042277d9c65b4fd1c02f41658c75414075e
phoziswa <phoziswanethe14@gmail.com>
2019-09-20 11:50:55 +0200

    added all files

---
+++ cricket_match.js
@@ -0,0 +1,15 @@
+module.export = function CricketScoreKeeper(){
+
+    var score = [];
+    var overs = [0,1, 2, 3, 4, 6, w ] || scored;
+    var wickets = 0;
+
+    function addScore(scored){
+     if(overs == 1 || 2 || 3 || 4 || 5 || 6){
+         score.push(over)
+     }
+    }
+    return{
+        addScore,
+    }
+}
---
+++ tests/cricket_match.test.js
@@ -0,0 +1,11 @@
+
+const assert = require('assert');
+const  CricketScoreKeeper = require('../cricket_match');
+
+describe('cricket_match', function(){
+
+     it('should add 1 to the score if the team get 1 score', function(){
+    
+        assert.equal(1, );
+     });
+});
